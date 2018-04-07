f0bec2c1eefe4b9773e0c2d4df9d74116a5e9dd1
SquidOfSpace <36462319+SquidOfSpace@users.noreply.github.com>
2018-04-07 00:18:56 -0600

    Code edit
    Modified the code to run less piezo speaker/photo resistors on one arduino and move the others to the other arduino

--- Cup_Keyboard_code.ino
+++ Cup_Keyboard_code.ino
@@ -10,14 +10,12 @@ const int photoPin = A0;
 const int photoPin2 = A1;
 const int photoPin3 = A2;
 const int photoPin4 = A3;
-const int photoPin5 = A4;
-const int photoPin6 = A5;
+
 const int piezo = 7;
 const int piezo2 = 6;
 const int piezo3 = 5;
 const int piezo4 = 4;
-const int piezo5 = 3;
-const int piezo6 = 2;
+
 //these constants are for the photoresistors(photopin) and the piezo buzzers(piezo)
 
 
@@ -26,8 +24,7 @@ int photo_value;
 int photo_value2;
 int photo_value3;
 int photo_value4;
-int photo_value5;
-int photo_value6;
+
 //(these variables are for the values returned by the photoresistors)
 
 void setup()
@@ -37,8 +34,6 @@ void setup()
   pinMode(piezo2, OUTPUT);
   pinMode(piezo3, OUTPUT);
   pinMode(piezo4, OUTPUT);
-  pinMode(piezo5, OUTPUT);
-  pinMode(piezo6, OUTPUT);
   //sets all the piezo buzzers to outputs
 
   Serial.begin(9600); // setting up the serial monitor
@@ -54,16 +49,14 @@ void loop()
   photo_value2 = analogRead(photoPin2);
   photo_value3 = analogRead(photoPin3);
   photo_value4 = analogRead(photoPin4);
-  photo_value5 = analogRead(photoPin5);
-  photo_value6 = analogRead(photoPin6);
+
 
   Serial.print("I just read: "); //Print the text 'i just read' on the serial monitor and also the photo values (helpful for adjusting thresholding)
   Serial.println(photo_value);
   Serial.println(photo_value2);
   Serial.println(photo_value3);
   Serial.println(photo_value4);
-  Serial.println(photo_value5);
-  Serial.println(photo_value6);
+
 
 //the if else statements for the activation of the piezo speakers
   {
@@ -90,9 +83,9 @@ void loop()
     }
     {
 
-      if (photo_value3 < 100)
+      if (photo_value3 < 80)
       {
-        tone(piezo3, NOTE_E6);
+        tone(piezo3, NOTE_E7);
       }
       else
       {
@@ -100,38 +93,16 @@ void loop()
       }
     }
     {
-      if (photo_value4 < 70)
+      if (photo_value4 < 80)
       {
-        tone(piezo4, NOTE_F6);
+        tone(piezo4, NOTE_F7);
       }
       else
       {
         noTone(piezo4);
       }
     }
-
-
-    {
-      if (photo_value5 < 70)
-      {
-        tone(piezo5, NOTE_G6);
-      }
-      else
-      {
-        noTone(piezo5);
-      }
-
-    }
-    {
-      if (photo_value6 < 70)
-      {
-        tone(piezo6, NOTE_A6);
-      }
-      else
-      {
-        noTone(piezo6);
       }
     }
-  }
-}
+
 
